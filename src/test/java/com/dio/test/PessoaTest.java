a098630ef1e324d658584f30187c22dd3b10b8d6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-12 23:39:05 -0300

    O básico para testar

--- src/test/java/com/dio/test/PessoaTest.java
+++ src/test/java/com/dio/test/PessoaTest.java
@@ -6,12 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+
 public class PessoaTest {
 
     @Test
-    void validaCalculoDeIdade() {
-        Pessoa pessoa = new Pessoa("Fulano", LocalDate.of(2020, 01, 01));
-        Assertions.assertEquals(2, pessoa.getIdade());
+    void deveCalcularIdadeCorretamente() {
+        Pessoa pessoa = new Pessoa("Fulano", LocalDate.of(2000, 01, 01));
+        Assertions.assertEquals(22, pessoa.getIdade());
+    }
+
+    @Test
+    void deveRetornaSeForMaiorDeIdade() {
+        Pessoa pessoa1 = new Pessoa("Fulano", LocalDate.of(2000, 01, 01));
+        Assertions.assertTrue(pessoa1.ehMaiorDeIdade());
+
+        Pessoa pessoa2 = new Pessoa("Beltrano", LocalDate.now());
+        Assertions.assertFalse(pessoa2.ehMaiorDeIdade());
     }
 
 }
